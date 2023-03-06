1b12b4b02d928ed39b18ebe4f0bdaad1af3da163
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-06 17:31:50 +0330

    Edit PhoneNumber class

---
+++ 04/phone_book/PhoneNumber.java
@@ -0,0 +1,15 @@
+public class PhoneNumber {
+    public PhoneNumber(String countryCode, String number) {
+        this.countryCode = countryCode;
+        this.number = number;
+    }
+
+
+    private String countryCode;
+    private String number;
+
+    public String toString() {
+        
+    }
+
+}
