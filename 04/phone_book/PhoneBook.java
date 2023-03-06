e09c885bf85bdeda9d45a05d170dfdeec9299fae
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-06 17:56:24 +0330

    Edit Contact class

---
+++ 04/phone_book/PhoneBook.java
@@ -0,0 +1,30 @@
+public class PhoneBook {
+    
+    public PhoneBook(Contact contact) {
+
+    }
+
+    private Contact[] contact;
+    
+
+    public boolean addContact(Contact newContact) {
+
+    }
+
+
+    public boolean deleteContact( ) {
+
+    } 
+
+    public Contact findContact() {
+        
+    }
+
+    public Contact[] findContacts() {
+        
+    }
+
+    public void printContacts() {
+
+    }
+}
