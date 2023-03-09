ef5dfd2d09a9700975fc74404de73e875a111ffe
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-09 05:05:16 +0330

    Edit PhoneBookController

--- 04/phonebook/controller/PhoneBookController.java
+++ 04/phonebook/controller/PhoneBookController.java
@@ -1,33 +1,116 @@
 package controller;
 
+import java.util.Scanner;
 import models.*;
+import views.*;
 import java.util.ArrayList;
 
 public class PhoneBookController {
     private PhoneBook phoneBook;
+    private PhoneBookView view;
+    private Scanner scanner;
 
-    public PhoneBookController(PhoneBook phoneBook) {
+    public PhoneBookController(PhoneBook phoneBook, PhoneBookView view) {
         this.phoneBook = phoneBook;
+        this.view = view;
+        scanner = new Scanner(System.in);
     }
 
-    public void addContact(String group, String email, String firstName, String lastName,
-        String countryCode, String number, String zipCode, String country, String city) {
+    public void handelMenu() {
+        int choice = -1;
+        while (choice != 0) {
+            view.printMenu();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice) {
+                case 1: 
+                    addContact();
+                    break;
+                case 2: 
+                    deleteContact();
+                    break;
+                case 3: 
+                    view.printContact(findContact());
+                    break;
+                case 4: 
+                    view.printContacts(findContacts());                    
+                    break;
+                case 5:
+                    view.printContacts(getAllContact());
+                    break; 
+                case 0:
+                    System.out.println("Goodby :)");
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+        }
+    }
+
+    public void addContact() {        
+        view.printFirstNameRequistMessage();
+        String firstName = scanner.nextLine();
+
+        view.printLastNameRequistMessage();
+        String lastName = scanner.nextLine();
+
+        view.printEmailRequistMessage();
+        String email = scanner.nextLine();
+
+        view.printGroupRequistMessage();
+        String group = scanner.nextLine();
+
+        view.printCountryCodeRequistMessage();
+        String countryCode = scanner.nextLine();
+
+        view.printPhoneNumberRequistMessage();
+        String number = scanner.nextLine();
+
+        view.printZipCodeRequistMessage();
+        String zipCode = scanner.nextLine();
+
+        view.printCountryRequistMessage();
+        String country = scanner.nextLine();
+
+        view.printCityRequistMessage();
+        String city = scanner.nextLine();
+
         Contact newContact = new Contact(group, email, firstName, lastName,
             new PhoneNumber(countryCode, number),
             new Address(zipCode, country, city));
         phoneBook.addContact(newContact);
     }
 
-    public void deleteContact(String firstName, String lastName) {
+    public void deleteContact() {
+        view.printFirstNameRequistMessage();
+        String firstName = scanner.nextLine();
+
+        view.printLastNameRequistMessage();
+        String lastName = scanner.nextLine();
+
         phoneBook.deleteContact(firstName, lastName);
     }
 
-    public Contact findContact(String firstName, String lastName) {
+    public Contact findContact() {
+        view.printFirstNameRequistMessage();
+        String firstName = scanner.nextLine();
+
+        view.printLastNameRequistMessage();
+        String lastName = scanner.nextLine();
+
         return phoneBook.findContact(firstName, lastName);
     }
 
-    public ArrayList<Contact> findContacts(String group) {
+    public ArrayList<Contact> findContacts() {
+        view.printGroupRequistMessage();
+        String group = scanner.nextLine();
+
         return phoneBook.findContacts(group);
+    }
+
+    public ArrayList<Contact> getAllContacts() {
+        return phoneBook.getAllContacts();
     }
 
     public ArrayList<Contact> getAllContact() {
