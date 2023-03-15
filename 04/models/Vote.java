3bcaa2a90e3242f1059d8552b0868eb91c7f73ec
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-15 19:15:19 +0330

    Constrauctor added to Vote class

--- 04/models/Vote.java
+++ 04/models/Vote.java
@@ -3,4 +3,9 @@ package models;
 public class Vote {
     private final Person voter;
     private final String date;
+
+    public Vote(Person voter, String date) {
+        this.voter = voter;
+        this.date = date;
+    }
 }
