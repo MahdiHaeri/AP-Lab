0254578118844aee26b4c5ce5325ab801c71787a
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-15 20:33:26 +0330

    Voting filds added

--- 04/models/Voting.java
+++ 04/models/Voting.java
@@ -1,5 +1,13 @@
 package models; 
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+
 public class Voting {
-    
+    private int type;
+    private String question;
+    private HashMap<String, HashSet<Vote>> choices;
+    private boolean isAnonymous;
+    private ArrayList<Person> voters;
 }
