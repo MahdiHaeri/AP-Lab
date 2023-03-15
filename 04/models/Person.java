3d02ded270d8b651224965d91c83aa07314f400c
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-15 18:59:03 +0330

    Constrauctor added to Person class

--- 04/models/Person.java
+++ 04/models/Person.java
@@ -4,4 +4,9 @@ public class Person {
     private String firstName;
     private String lastName;
 
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
 }
