65a73ae7a27c0bf7edce983b0ea7ccf356844a11
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-16 12:58:37 +0330

    Printing methods added to VotingSystem class

--- 04/models/VotingSystem.java
+++ 04/models/VotingSystem.java
@@ -26,4 +26,39 @@ public class VotingSystem {
 
       votingList.add(newVoting);
    }
+
+   public void printResult(int index) {
+      if (index <= 0 || index >= votingList.size()) {
+         System.out.println("There is no such index in voting list.");
+         return;
+      }
+
+      System.out.println("Voting " + index);
+      votingList.get(index).printResult();
+   }
+
+   public void printVoters(int index) {
+      if (index <= 0 || index >= votingList.size()) {
+         System.out.println("There is no such index in voting list.");
+         return;
+      }
+
+      System.out.println("voters " + index);
+      votingList.get(index).printVoters();
+   }
+
+   public void printVoting(int index) {
+      if (index <= 0 || index >= votingList.size()) {
+         System.out.println("There is no such index in voting list.");
+         return;
+      }
+      
+      System.out.println(votingList.get(index).getQuestion());
+      System.out.println("--------------------------------------------");
+      ArrayList<String> choices = votingList.get(index).getChoices();
+
+      for (int i = 0; i < choices.size(); i++) {
+         System.out.println(i + ": " + choices.get(i));
+      }
+   }
 }
