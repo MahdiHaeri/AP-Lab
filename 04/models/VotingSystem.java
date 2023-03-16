efccbcf1282ceb0a2a1fc0ba160f50fded1dcb25
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-16 12:44:11 +0330

    createVoting method added to VotingSystem class

--- 04/models/VotingSystem.java
+++ 04/models/VotingSystem.java
@@ -8,4 +8,14 @@ public class VotingSystem {
    public VotingSystem() {
 
    }
+
+   public void createVoting(String question, boolean isAnonymous, int type, ArrayList<String> choices) {
+      Voting newVoting = new Voting(type, question, isAnonymous);
+      
+      for (String choice: choices) {
+         newVoting.createChoice(choice);
+      }
+
+      votingList.add(newVoting);
+   }
 }
