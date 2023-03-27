8e95b36e714a28a52a8a0b493ac18d1ec067018f
mohammad-mahdi-haeri <mahdihaerim@gmail.com>
2023-03-28 02:28:37 +0330

    equals and hashCode method added to Vote class

--- 04/models/Vote.java
+++ 04/models/Vote.java
@@ -11,4 +11,28 @@ public class Vote {
 
     public Person getVoter() { return voter; }
     public String getDate() { return date; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof Vote)) {
+            return false;
+        }
+
+        Vote vote = (Vote) obj;
+
+        return vote.getVoter().equals(this.getVoter()) && vote.getDate().equals(this.getDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return voter.hashCode() + date.hashCode();
+    }
 }
