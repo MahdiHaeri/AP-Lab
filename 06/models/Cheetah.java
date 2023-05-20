6d7eb261f692a6b71eb86e546ce3c25d69c27f95
Mohammad Mahdi Haeri <mahdi@Mahdis-Laptop.local>
2023-05-20 10:47:01 +0330

    Add Parrot, Cheetah classes

---
+++ 06/models/Cheetah.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Cheetah extends Mammal {
+    public Cheetah(String name, int age, String attributes, double speed) {
+        super(name, age, attributes, speed);
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ 06/models/Parrot.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Parrot extends Bird {
+    public Parrot(String name, int age, String attributes, double heightOfFlight) {
+        super(name, age, attributes, heightOfFlight);
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
