90963ef016ad768bfc029e2edead1ca81bfe6a0e
Mohammad Mahdi Haeri <mahdi@Mahdis-Laptop.local>
2023-05-20 15:19:09 +0330

    add Main.java

---
+++ 06/Main.java
@@ -0,0 +1,26 @@
+import models.*;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Parrot parrot = new Parrot("parrot", 10, "beautiful", 10);
+        Eagle eagle = new Eagle("eagle", 100, "alone", 1000);        
+        Girafe girafe = new Girafe("griafe", 20, "long",20);
+        Cheetah cheetah = new Cheetah("cheetah", 5, "fastest",80);
+
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+
+        animals.add(parrot);
+        animals.add(eagle);
+        animals.add(girafe);
+        animals.add(cheetah);
+
+        for (Animal animal: animals) {
+            animal.show();
+        }
+
+        eagle.hunt(parrot);
+        cheetah.hunt(girafe);
+    }
+}
