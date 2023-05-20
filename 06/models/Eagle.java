07d40f6bd08bb6c2e09d8a186891c5782464acac
Mohammad Mahdi Haeri <mahdi@Mahdis-Laptop.local>
2023-05-20 11:17:40 +0330

    Add Eagle, Girafe classes

---
+++ 06/models/Eagle.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Eagle extends Bird {
+    public Eagle (String name, int age, String attributes, double heightOfFlight) {
+        super(name, age, attributes, heightOfFlight);
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ 06/models/Girafe.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Girafe extends Mammal {
+    public Girafe (String name, int age, String attributes, double speed) {
+        super(name, age, attributes, speed);
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
