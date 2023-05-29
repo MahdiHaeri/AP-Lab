9c0982a3ad76d0677db42d700101fe780cf21e73
Mohammad Mahdi Haeri <mahdi@Mahdis-Laptop.local>
2023-05-30 01:03:22 +0330

    Init ChatRoom, Add user

---
+++ 10/ChatRoom/src/main/java/org/example/server/models/User.java
@@ -0,0 +1,24 @@
+package org.example.server.models;
+
+public class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
